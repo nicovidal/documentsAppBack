5754b33295d6ad31a4d16414253e6cca97fe8b29
root <root@prueba-deploy.4xzshkx10m3eroo5d2hefmbnaa.xx.internal.cloudapp.net>
2024-11-25 20:38:23 +0000

    cambio

--- database/config.js
+++ database/config.js
@@ -6,7 +6,10 @@ const pool = new Pool({
   host: process.env.DB_HOST,        
   database: process.env.DB_NAME,     
   password:process.env.DB_PASSWORD,  
-  port:process.env.DB_PORT,  
+  port: parseInt(process.env.DB_PORT, 10),
+    ssl: {
+    rejectUnauthorized: false 
+  }
 });
 
 const dbConnection = async () => {
