0c67addb20aecd512d1deac5df95a0212e1e2585
nicovidal <nico.vidal.m@gmail.com>
2024-11-25 16:30:30 -0300

    intennto

--- database/config.js
+++ database/config.js
@@ -9,7 +9,12 @@ const pool = new Pool({
   port:process.env.DB_PORT,  
 });
 
-console.log(DB_USER)
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_NAME:', process.env.DB_NAME);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+console.log('DB_PORT:', process.env.DB_PORT);
+
 const dbConnection = async () => {
   try {
 
