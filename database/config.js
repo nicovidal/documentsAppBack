3b4a96278a44781a3bd319cc145917c7fa1fc534
nicovidal <nico.vidal.m@gmail.com>
2024-11-25 17:22:03 -0300

    config

--- database/config.js
+++ database/config.js
@@ -9,12 +9,6 @@ const pool = new Pool({
   port:process.env.DB_PORT,  
 });
 
-console.log('DB_USER:', process.env.DB_USER);
-console.log('DB_HOST:', process.env.DB_HOST);
-console.log('DB_NAME:', process.env.DB_NAME);
-console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
-console.log('DB_PORT:', process.env.DB_PORT);
-
 const dbConnection = async () => {
   try {
 
