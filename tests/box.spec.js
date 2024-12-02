1ed756de3b7eb7bfc838f30e785ea6a0b658d4dd
nicovidal <nico.vidal.m@gmail.com>
2024-12-02 17:06:57 -0300

    test

--- tests/box.spec.js
+++ tests/box.spec.js
@@ -1,4 +1,4 @@
-const request = require('supertest')
+/* const request = require('supertest')
 const app = require('../routes/documents')
 
 describe('GET /documents',()=>{
@@ -14,4 +14,4 @@ describe('GET /documents',()=>{
     })
 
 
-})
+}) */
