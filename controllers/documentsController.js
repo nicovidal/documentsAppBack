e7b96d32bf1c39d482079c82f93c777668773e7d
nicovidal <nico.vidal.m@gmail.com>
2024-12-02 13:38:57 -0300

    avance get con test

--- controllers/documentsController.js
+++ controllers/documentsController.js
@@ -2,14 +2,9 @@ const {sequelize}=require('../database/config')
 const Document=require('../models/documents')
 
 
-
-
 const getDocuments=async (req,res)=>{
-
     try {
-
         const documents=await Document.findAll()
-
         if (!documents || documents.length === 0) {
             return res.status(404).json({
                 ok: false,
@@ -21,14 +16,11 @@ const getDocuments=async (req,res)=>{
         res.status(200).json({
             ok:true,
             documents
-        })
-        
+        })     
     } catch (error) {
        console.log(error)
     }
 }
-
-
 
 
 
--- tests/document.spec.js
+++ tests/document.spec.js
@@ -4,7 +4,13 @@ const app = require('../routes/prueba')
 describe('GET /documents',()=>{
 
     test('debe retonar un 200 ',async()=>{
-        const response = await request()
+        const response = await request(app).get('/').send()
+        expect(response.statusCode).toBe(200)
+    })
+
+    test('debe retonar un 404 ',async()=>{
+        const response = await request(app).get('/').send()
+        expect(response.statusCode).toBe(404)
     })
 
 
