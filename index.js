87f3eacdd18210e5887d3139cb1dc0fd1b12dcaf
nicovidal <nico.vidal.m@gmail.com>
2024-12-04 15:33:18 -0300

    ok cors

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const cors = require('cors'); 
 require('dotenv').config();
 const { dbConnection, sequelize } = require('./database/config');
 const app = express();
+
+app.use(cors({
+  origin:  '*', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+  allowedHeaders: ['Content-Type', 'Authorization'], 
+}));
+
 
 // Modelos
 const Document = require('./models/documents');
